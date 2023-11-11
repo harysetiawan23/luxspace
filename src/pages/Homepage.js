435640aa608d2137f5f185973dc954143c5fa86d
harysetiawan23 <setiawan230896@gmail.comm>
2023-11-11 11:55:14 +0800

    feat: add custom hooks

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import Header from 'parts/Header';
-import Hero from 'parts/Hero';
+import Hero from 'parts/Homepage/Hero';
 import JustArrive from 'parts/Homepage/JustArrive';
 import BrowseRoom from 'parts/Homepage/BrowseRoom';
 import Clients from 'parts/Clients';
 import Sitemap from 'parts/Sitemap';
 import Footer from 'parts/Footer';
+import useScrollAnchor from 'helpers/hooks/useScrollAnchor';
+import useModalDOM from 'helpers/hooks/useModalDOM';
 export default function Homepage(props) {
+  useScrollAnchor();
+  useModalDOM()
+
   return (
     <>
       <Header theme="white" position="absolute"></Header>
